4a2456fa4476eeb3a3fa9142f0be81364aa99818
StefanCiotec <stefan.ciotec@gmail.com>
2015-04-17 09:44:04 +0300

    all resolved

--- minfs/minfs.c
+++ minfs/minfs.c
@@ -30,6 +30,36 @@ struct minfs_inode_info {
 	struct inode vfs_inode;
 };
 
+struct inode *myfs_get_inode(struct super_block *sb, int mode)
+{
+	struct inode *inode = new_inode(sb);
+
+	if (!inode)
+		return NULL;
+
+	/* TODO 3: fill inode structure
+	 *     - mode
+	 *     - uid
+	 *     - gid
+	 *     - atime,ctime,mtime
+	 */
+	inode_init_owner(inode, NULL, mode);
+	inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+	
+	/*
+	 * TODO 3: inode operations for root inode (use S_ISDIR macro)
+	 *     directory link count should be incremented (use inc_nlink)
+	 */
+	 if (S_ISDIR(mode)) {
+	 	inode->i_op = &simple_dir_inode_operations;
+	 	inode->i_fop = &simple_dir_operations;
+	 	inc_nlink(inode);
+	 }
+
+	return inode;
+}
+
+
 static struct inode *minfs_iget(struct super_block *s, unsigned long ino)
 {
 	struct minfs_inode *mi;
@@ -45,10 +75,26 @@ static struct inode *minfs_iget(struct super_block *s, unsigned long ino)
 	}
 
 	/* TODO 4: read disk inode block (2nd block; index 1) */
+	if (!(bh = sb_bread(s, 1)))
+		goto out_bad_sb;
+
+	mi = (struct minfs_inode *) bh->b_data;
 
 	/* TODO 4: interpret disk inode as minfs_inode */
 
 	/* TODO 4: fill VFS inode */
+	inode->i_uid = mi->uid; 
+	inode->i_gid = mi->gid;
+	inode->i_mode = mi->mode;
+	inode->i_size = mi->size;
+
+	 if (S_ISDIR(mi->mode)) {
+		 inode->i_op = &simple_dir_inode_operations;
+		inode->i_fop = &simple_dir_operations;
+		inc_nlink(inode);
+	 }
+	inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+
 
 	/* fill data for mii */
 	mii = container_of(inode, struct minfs_inode_info, vfs_inode);
@@ -65,11 +111,18 @@ out_bad_sb:
 	return NULL;
 }
 
+static struct kmem_cache * minfs_inode_cachep;
+
 static struct inode *minfs_alloc_inode(struct super_block *s)
 {
 	struct minfs_inode_info *mii;
 
 	/* TODO 3: allocate minfs_inode_info */
+	mii = (struct minfs_inode_info *)kzalloc(sizeof(*mii), GFP_KERNEL);
+	if (!mii)
+		return NULL;
+
+	inode_init_once(&mii->vfs_inode);
 
 	return &mii->vfs_inode;
 }
@@ -77,11 +130,18 @@ static struct inode *minfs_alloc_inode(struct super_block *s)
 static void minfs_destroy_inode(struct inode *inode)
 {
 	/* TODO 3: free minfs_inode_info */
+	struct minfs_inode_info *mmi;
+
+	mmi = container_of(inode, struct minfs_inode_info, vfs_inode);
+
+	kfree(mmi);
 }
 
 static const struct super_operations minfs_ops = {
 	.statfs		= simple_statfs,
 	/* TODO 4: add alloc and destroy inode functions */
+	.alloc_inode    = minfs_alloc_inode,
+	.destroy_inode  = minfs_destroy_inode,
 };
 
 static int minfs_fill_super(struct super_block *s, void *data, int silent)
@@ -103,20 +163,32 @@ static int minfs_fill_super(struct super_block *s, void *data, int silent)
 		goto out_bad_blocksize;
 
 	/* TODO 2: read block with superblock (1st block, index 0) */
+	if (!(bh = sb_bread(s, 0)))
+		goto out_bad_sb;
 
 	/* TODO 2: interpret read data as minfs_super_block */
+	ms = (struct minfs_super_block *) bh->b_data;
 
 	/* TODO 2: check magic number; jump to out_bad_magic if not suitable */
+	s->s_magic = ms->magic;
+	if (s->s_magic != MINFS_MAGIC)
+		goto out_bad_magic;
 
 	/* TODO 2: fill sbi with information from disk superblock */
+	sbi->version = ms->version;
 
 	/* TODO 2: fill super_block with magic_number, super_operations */
+	s->s_magic = MINFS_MAGIC;
+	s->s_op = &minfs_ops;
 
 	/* allocate root inode and root dentry */
 	/* TODO 2: use myfs_get_inode instead of minfs_iget */
 	root_inode = minfs_iget(s, 0);
+	//root_inode = myfs_get_inode(s, S_IFDIR | S_IRWXU | S_IRGRP |
+			//S_IXGRP | S_IROTH | S_IXOTH);	
 	if (!root_inode)
 		goto out_bad_inode;
+	root_inode->i_ino = 1;
 
 	root_dentry = d_make_root(root_inode);
 	if (!root_dentry)
@@ -147,12 +219,17 @@ static struct dentry *minfs_mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data)
 {
 	/* TODO 1: call superblock mount function */
+	return mount_bdev(fs_type, flags, dev_name, data, minfs_fill_super);
 }
 
 static struct file_system_type minfs_fs_type = {
 	.owner = THIS_MODULE,
 	.name = "minfs",
 	/* TODO 1: add mount, kill_sb and fs_flags */
+	.mount	= minfs_mount,
+	.kill_sb = kill_block_super,
+	.fs_flags = FS_REQUIRES_DEV, 
+
 };
 
 static int __init minfs_init(void)
